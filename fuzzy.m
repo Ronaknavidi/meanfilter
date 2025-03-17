d7b723ac1beacf97d91bfc2d95bd68fa8b77e9bd
Ronaknavidi <ronak.navidi2018@gmail.com>
2025-03-17 09:06:30 +0330

    Add files via upload

---
+++ fuzzy.m
@@ -0,0 +1,20 @@
+﻿% خواندن تصویر
+img = imread('sample.png');
+img = im2double(img);  % تبدیل به تصویر با مقادیر اعشاری بین 0 و 1
+
+% ایجاد فیلتر 3x3 برای میانگین‌گیری فازی
+fuzzy_mean_filter = ones(3, 3) / 9;
+
+% اعمال فیلتر روی تصویر با استفاده از imfilter
+output_img = imfilter(img, fuzzy_mean_filter, 'symmetric');
+
+% نمایش تصویر اصلی و تصویر فیلتر شده
+figure;
+subplot(1, 2, 1);
+imshow(img);
+title('تصویر اصلی');
+
+subplot(1, 2, 2);
+imshow(output_img);
+title('تصویر فیلتر شده با میانگین‌گیری فازی');
+
